131fb72a9c6f2775cc119ce294662b23c3e4bc89
rmk <rmk@bistromath.org>
2017-03-26 13:25:15 -0400

    Add config sync

---
+++ lib/puppet/provider/f5_devicegroup/f5_devicegroup.rb
@@ -0,0 +1,59 @@
+require 'puppet/provider/f5'
+
+Puppet::Type.type(:f5_devicegroup).provide(:f5_devicegroup, :parent => Puppet::Provider::F5) do
+    @doc = "Manages F5 devicegroup"
+
+    confine        :feature => :posix
+    defaultfor     :feature => :posix
+
+    def wsdl
+        self.class.wsdl
+    end
+
+    def create
+        # TODO - implement hostgroup create/destroy
+    end
+
+    def destroy
+        # TODO - implement hostgroup create/destroy
+    end
+
+    def exists?
+        true
+    end
+
+    def config_ensure
+        sync_status[:member_state].downcase.sub /^member_state_/, ''
+    end
+
+    def config_ensure=(value)
+        api_call 'System.ConfigSync', :synchronize_to_group_v2, { group: resource[:name], device: sync_source_device,  force: 'false' }
+    end
+
+    def device
+        @device ||= api_call 'Management.Device', :get_local_device
+    end
+
+    def sync_status
+        @sync_status ||= api_call('Management.DeviceGroup', :get_sync_status, { device_groups: { item: [ resource[:name] ] } })
+    end
+
+    def device_sync_status
+        @device_sync_status ||= begin
+            devices = api_call('Management.DeviceGroup', :get_device, { device_groups: { item: [ resource[:name] ] } })
+            statuses = api_call('Management.DeviceGroup', :get_device_sync_status, { device_groups: { item: [ resource[:name] ] }, devices: { item: { item: devices } } })
+            Hash[devices.zip(statuses)]
+        end
+    end
+
+    def sync_source_device
+        @sync_source_device ||= begin
+            device_sync_status.keys.max_by do |name|
+                time = device_sync_status[name][:commit_id][:sync_time]
+                DateTime.iso8601 sprintf("%04d-%02d-%02dT%02d:%02d:%02d",
+                    time[:year], time[:month], time[:day], time[:hour], time[:minute], time[:second])
+            end
+        end
+    end
+
+end
---
+++ lib/puppet/type/f5_devicegroup.rb
@@ -0,0 +1,24 @@
+Puppet::Type.newtype(:f5_devicegroup) do
+    @doc = "F5 device group"
+
+    feature :descriptions, "Manages an F5 device group. Manages manual configsync operations when auto-sync disabled"
+
+    apply_to_device
+
+    ensurable do
+        newvalue(:present) do
+            provider.create
+        end
+        defaultto :present
+    end
+
+    newparam(:name, :namevar=>true) do
+        desc "The group name"
+        #newvalues(/^[[:alpha:][:digit:]]+$/)
+    end
+
+    newproperty(:config_ensure) do
+        desc "Set to :synched to ensure config is synched"
+    end
+
+end
